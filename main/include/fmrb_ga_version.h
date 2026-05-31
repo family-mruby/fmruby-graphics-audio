74f0d343b7af64683441a02d4224dc026d71a1ac
kishima <kishima7@gmail.com>
2026-06-01 07:52:46 +0900

    update link ver 4

--- main/include/fmrb_ga_version.h
+++ main/include/fmrb_ga_version.h
@@ -4,4 +4,4 @@
 #define FMRB_GA_FW_VERSION "1.0.0"
 
 // Link protocol version (GA side's local value; must match Core's FMRB_LINK_VERSION in fmrb.h)
-#define FMRB_LINK_VERSION 3
+#define FMRB_LINK_VERSION 4
