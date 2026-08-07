f5728b43e494d190da8812483b0fc29b7537da08
kishima <kishima7@gmail.com>
2026-08-07 22:14:08 +0900

    set version 2.0.0

--- main/include/fmrb_ga_version.h
+++ main/include/fmrb_ga_version.h
@@ -1,7 +1,7 @@
 #pragma once
 
 // GA firmware version (this build)
-#define FMRB_GA_FW_VERSION "1.0.0"
+#define FMRB_GA_FW_VERSION "2.0.0"
 
 // Link protocol version (GA side's local value; must match Core's FMRB_LINK_VERSION in fmrb.h)
 #define FMRB_LINK_VERSION 4
